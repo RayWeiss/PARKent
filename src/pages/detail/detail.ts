5e65513bb463221bd2675b581ff61cdce8dfa57d
RayWeiss <raymondjohnweiss@gmail.com>
2017-10-04 20:49:16 -0400

    added details page, list navigates to detail page

---
+++ src/pages/detail/detail.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the DetailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-detail',
+  templateUrl: 'detail.html',
+})
+export class DetailPage {
+  item: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.item = navParams.get('item');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetailPage');
+  }
+
+}
