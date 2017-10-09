751ae3b34d5a59b6727f06abefb7e9e638f93501
RayWeiss <raymondjohnweiss@gmail.com>
2017-10-09 11:09:14 -0400

    added provider for rest, modified map view and readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { DbRestServiceProvider } from '../providers/db-rest-service/db-rest-service';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { MyApp } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -21,7 +24,8 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DbRestServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DbRestServiceProvider } from '../../providers/db-rest-service/db-rest-service';
 
 declare var google;
 
@@ -7,13 +8,16 @@ declare var google;
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
+  providers: [DbRestServiceProvider]
 })
 export class MapPage {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  db: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: DbRestServiceProvider) {
+    this.db = dbService;
   }
 
   ionViewDidLoad() {
@@ -24,8 +28,17 @@ export class MapPage {
   initMap() {
     this.map = new google.maps.Map(this.mapElement.nativeElement, {
       zoom: 16,
-      center: {lat: 41.147799193274636, lng: -81.34373903024539}
+      center: {lat: 41.147799193274636, lng: -81.34373903024539},
+      streetViewControl: false,
+      mapTypeControl: false,
+      clickableIcons: false,
     });
+    this.map.setOptions({styles: [
+      {
+        featureType: 'poi',
+        stylers: [{visibility: 'off'}]
+      }
+    ]});
   }
 
 }
