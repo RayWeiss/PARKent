4d0646402066c6e916d9255116278f06b9f613c6
RayWeiss <raymondjohnweiss@gmail.com>
2017-10-09 22:29:57 -0400

    map calls rest service and renders pins

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -15,6 +15,8 @@ export class MapPage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   db: any;
+  markers = {};
+  spots: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: DbRestServiceProvider) {
     this.db = dbService;
@@ -23,11 +25,12 @@ export class MapPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
     this.initMap();
+    this.initializeMarkers();
   }
 
   initMap() {
     this.map = new google.maps.Map(this.mapElement.nativeElement, {
-      zoom: 16,
+      zoom: 15,
       center: {lat: 41.147799193274636, lng: -81.34373903024539},
       streetViewControl: false,
       mapTypeControl: false,
@@ -39,6 +42,28 @@ export class MapPage {
         stylers: [{visibility: 'off'}]
       }
     ]});
+  }
+
+  initializeMarkers() {
+    this.db.getSpots()
+    .then(data => {
+      this.spots = data;
+      for (var key in this.spots) {
+        var spot = this.spots[key]
+        var name = spot[0];
+        var totalSpots = spot[1];
+        var lat = parseFloat(spot[2]);
+        var lon = parseFloat(spot[3]);
+        var url = spot[4];
+
+        var marker = new google.maps.Marker({
+          position: {lat: lat, lng: lon},
+          map: this.map
+        });
+
+        this.markers[name] = marker;
+      }
+    });
   }
 
 }
--- src/providers/db-rest-service/db-rest-service.ts
+++ src/providers/db-rest-service/db-rest-service.ts
@@ -11,21 +11,45 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class DbRestServiceProvider {
   response: any;
+  data: any;
 
   constructor(public http: Http) {
-    this.response = this.http.get('http://127.0.0.1:5000/allLots');
-    this.response.subscribe(
-        data => {
-            console.log(data);
-            console.log(data._body)
-            // console.log(data.data);
-            // this.response = data.data.children;
-        },
-        err => {
-            console.log("Oops!");
-        }
-    );
     console.log('Hello DbRestServiceProvider Provider');
   }
 
+  getSpots() {
+    if (this.data) {
+        // already loaded data
+        return Promise.resolve(this.data);
+      }
+      // don't have the data yet
+      return new Promise(resolve => {
+        // We're using Angular HTTP provider to request the data,
+        // then on the response, it'll map the JSON data to a parsed JS object.
+        // Next, we process the data and resolve the promise with the new data.
+        this.http.get('http://127.0.0.1:5000/allLots')
+          .map(res => res.json())
+          .subscribe(data => {
+            // we've got back the raw data, now generate the core schedule data
+            // and save the data for later reference
+            this.data = data;
+            resolve(this.data);
+          });
+      });
+    }
+
+    // getStuff() {
+    //   this.response = this.http.get('http://127.0.0.1:5000/allLots');
+    //   this.response.subscribe(
+    //       data => {
+    //           // console.log(data);
+    //           console.log(data._body)
+    //           // console.log(data.data);
+    //           // this.response = data.data.children;
+    //       },
+    //       err => {
+    //           console.log("Oops!");
+    //       }
+    //   );
+    // }
 }
