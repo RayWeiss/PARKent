8694a1e30eac21c6bd0e32406f37495b8a767d62
RayWeiss <raymondjohnweiss@gmail.com>
2017-10-24 20:52:57 -0400

    map now updating marker colors from DB values

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,11 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DbRestServiceProvider } from '../../providers/db-rest-service/db-rest-service';
+import {} from '@types/googlemaps';
+import { Observable } from 'rxjs/Rx';
+
+
+
 
 declare var google;
 
@@ -15,12 +20,12 @@ export class MapPage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   db: any;
-  markers = {};
-  spots: any;
-
+  markers = new Map<string, google.maps.Marker>();
   green = "00CC00";
   yellow = "FFFF00";
   red = "FF0000";
+  gray = "808080";
+  black = "000000";
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: DbRestServiceProvider) {
     this.db = dbService;
@@ -30,6 +35,7 @@ export class MapPage {
     console.log('ionViewDidLoad MapPage');
     this.initMap();
     this.initializeMarkers();
+    this.continuallyUpdateMarkers();
   }
 
   initMap() {
@@ -50,25 +56,26 @@ export class MapPage {
 
   initializeMarkers() {
     this.db.getSpots()
-    .then(data => {
-      this.spots = data;
-      for (var key in this.spots) {
-        var spot = this.spots[key]
+    .then(spots => {
+      // this.spots = data;
+      for (var key in spots) {
+        var spot = spots[key]
         var name = spot[0];
-        var totalSpots = spot[1];
+        // var totalSpots = spot[1];
         var lat = parseFloat(spot[2]);
         var lon = parseFloat(spot[3]);
-        var url = spot[4];
+        // var url = spot[4];
 
         var marker = new google.maps.Marker({
           position: {lat: lat, lng: lon},
           map: this.map
         });
-        marker.setIcon(this.getPinWithHexColor(this.red));
-        // marker.setIcon(this.getCircleWithHexColor(this.green));
-        this.markers[name] = marker;
+        marker.setIcon(this.getPinWithHexColor(this.gray));
+        var markerAsMarkerObject = marker as google.maps.Marker;
+        this.markers.set(name, markerAsMarkerObject);
       }
     });
+
   }
 
   getPinWithHexColor(color) {
@@ -88,7 +95,57 @@ export class MapPage {
     }
   }
 
-  changeLotMarkerToColor(lotName, color) {
-    // this.markers[lotName].setIcon(this.getPinWithHexColor(color));
+  continuallyUpdateMarkers() {
+    const seconds = 5;
+    const interval = Observable.interval(seconds * 1000);
+    const forever = interval.filter(val => false);
+
+    interval.takeUntil(forever).subscribe(val => {
+      var allSpots = [];
+      var allStatus = [];
+      this.db.getSpots()
+      .then(spots => {
+        for (var key in spots) {
+          var spot = spots[key]
+          var name = spot[0];
+          allSpots.push(name);
+        }
+      }).then(spots => {
+        for(var spot in allSpots){
+          var name = allSpots[spot];
+
+          this.db.getStatus(name)
+          .then(status => {
+            for (var key in status) {
+              var currentStatus = status[key]
+              var percentLeft = Number(currentStatus[0]);
+              allStatus.push(percentLeft);
+            }
+            if(allStatus.length == allSpots.length){
+              for(var i = 0; i < allSpots.length; ++i) {
+                var nameToUpdate = allSpots[i];
+                var percentToUpdateWith = allStatus[i];
+
+                var color;
+
+                if(percentToUpdateWith > 0.25) {
+                  color = this.green;
+                } else if (percentToUpdateWith > 0.10) {
+                  color = this.yellow;
+                } else if (percentToUpdateWith > 0.0) {
+                  color = this.red;
+                } else {
+                  color = this.black;
+                }
+
+                this.markers.get(nameToUpdate).setIcon(this.getPinWithHexColor(color));
+              }
+              allSpots = [];
+              allStatus = [];
+            }
+          });
+        }
+      });
+    });
   }
 }
--- src/providers/db-rest-service/db-rest-service.ts
+++ src/providers/db-rest-service/db-rest-service.ts
@@ -12,16 +12,18 @@ import 'rxjs/add/operator/map';
 export class DbRestServiceProvider {
   response: any;
   data: any;
+  getSpotsResponse: any;
+  getStatusResponse: any;
 
   constructor(public http: Http) {
     console.log('Hello DbRestServiceProvider Provider');
   }
 
   getSpots() {
-    if (this.data) {
-        // already loaded data
-        return Promise.resolve(this.data);
-      }
+    // if (this.getSpotsResponse) {
+    //     // already loaded data
+    //     return Promise.resolve(this.getSpotsResponse);
+    //   }
       // don't have the data yet
       return new Promise(resolve => {
         // We're using Angular HTTP provider to request the data,
@@ -32,12 +34,32 @@ export class DbRestServiceProvider {
           .subscribe(data => {
             // we've got back the raw data, now generate the core schedule data
             // and save the data for later reference
-            this.data = data;
-            resolve(this.data);
+            this.getSpotsResponse = data;
+            resolve(this.getSpotsResponse);
           });
       });
     }
 
+    getStatus(lotName) {
+      // if (this.getStatusResponse) {
+      //     // already loaded data
+      //     return Promise.resolve(this.getStatusResponse);
+      //   }
+        // don't have the data yet
+        return new Promise(resolve => {
+          // We're using Angular HTTP provider to request the data,
+          // then on the response, it'll map the JSON data to a parsed JS object.
+          // Next, we process the data and resolve the promise with the new data.
+          this.http.get('http://127.0.0.1:5000/percentleft/' + lotName)
+            .map(res => res.json())
+            .subscribe(data => {
+              // we've got back the raw data, now generate the core schedule data
+              // and save the data for later reference
+              this.getStatusResponse = data;
+              resolve(this.getStatusResponse);
+            });
+        });
+      }
     // getStuff() {
     //   this.response = this.http.get('http://127.0.0.1:5000/allLots');
     //   this.response.subscribe(
