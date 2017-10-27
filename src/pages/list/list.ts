fb4acc6a294c86912b7254a0f8f23c60b123cbaf
RayWeiss <raymondjohnweiss@gmail.com>
2017-10-26 20:28:32 -0400

    list page pulls lot names from sevice

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DbRestServiceProvider } from '../../providers/db-rest-service/db-rest-service';
 
 @IonicPage()
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html',
+  providers: [DbRestServiceProvider]
 })
 export class ListPage {
   items: any[];
+  db: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: DbRestServiceProvider) {
+    this.db = dbService;
     this.items = [];
-    for(let i = 0; i < 10; ++i) {
-      this.items.push({
-        text: 'Lot ' + i,
-        id: i
-      });
-    }
+    this.getLotNames();
   }
 
   ionViewDidLoad() {
@@ -26,6 +25,21 @@ export class ListPage {
   itemSelected(item) {
     this.navCtrl.push('DetailPage', {
       item: item
+    });
+  }
+
+  getLotNames() {
+    this.db.getSpots()
+    .then(spots => {
+      for (var key in spots) {
+        var spot = spots[key]
+        var lotName = spot[0];
+        var totalSpots = spot[1];
+        this.items.push({
+          lotName: lotName,
+          totalSpots: totalSpots
+        });
+      }
     });
   }
 
